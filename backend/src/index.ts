8923de565183d0f564f9f6fc8851e63e8d4bce1d
aadarsh214 <aadarshgupta7828@gmail.com>
2025-01-01 20:12:35 +0530

    initalized prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,24 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/', (c) => {                   //c => means context , it contains req , res , next , error
   return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/signup', (c) => {
+  return c.text('Signup Success')
+})
+app.post('/api/v1/signin', (c) => {
+  return c.text('Signin Success')
+})
+app.post('/api/v1/blog', (c) => {
+  return c.text('blog')
+})
+app.put('/api/v1/blog', (c) => {
+  return c.text('blog')
+})
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('blog')
 })
 
 export default app
