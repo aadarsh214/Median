e19f815a9b5c0b526f6ba9348d524ed87a2ac28c
aadarsh214 <aadarshgupta7828@gmail.com>
2025-01-02 16:26:59 +0530

    keywords fixed and

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import {PrismaClient} from '@prisma/client/edge'
+import {PrismaClient} from '@prisma/client/edge.js'
 import {withAccelerate} from '@prisma/extension-accelerate'
 import {sign} from 'hono/jwt'
 import { SignatureKey } from 'hono/utils/jwt/jws'
