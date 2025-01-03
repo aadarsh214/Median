79b48c0fd67c1e978d8147ad47f244615ef29733
aadarsh214 <aadarshgupta7828@gmail.com>
2025-01-03 13:41:33 +0530

    sign in routed added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,8 +39,25 @@ app.post('/api/v1/signup', async (c) => {
 });
 
 
-app.post('/api/v1/signin', (c) => {
-  return c.text('Signin Success')
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body =  await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: { 
+      email: body.email,
+      password: body.password,
+     },
+  });
+  if(!user){
+    c.status(403)
+    return c.json({ error: 'user not found' }, 403);
+  }
+
+  const jwt = await sign({id: user.id}, c.env.JWT_SECRET as string)
+  return c.json({ jwt },200)
 })
 app.post('/api/v1/blog', (c) => {
   return c.text('blog')
