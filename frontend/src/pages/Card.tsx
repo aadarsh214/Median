831e01f9e96f4c641de9653a702536d4b76146be
aadarsh214 <aadarshgupta7828@gmail.com>
2025-01-06 12:41:03 +0530

    card

--- frontend/src/pages/Card.tsx
+++ frontend/src/pages/Card.tsx
@@ -1,5 +1,13 @@
 export const Card = () =>{
-    return <div>
-        card
+    return <div className=" bg-gray-50 flex flex-col text-center justify-center h-screen">
+        <div className="flex justify-center">
+        <div className="max-w-md font-bold text-2xl">
+        The only way to do great work is to love what you do
+        </div>
+        <div className="text-left font-thin max-w-md">
+            <h5>CEO</h5>
+            <h4>Steve Jobs</h4>
+        </div>
+        </div>
     </div>
 }
